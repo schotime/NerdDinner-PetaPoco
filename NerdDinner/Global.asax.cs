2951ab5ec5b879ca635c3fcf923ac4e1a1160763
Adam Schroder <adamschroder@gmail.com>
2011-09-01 13:21:20 +1000

    Initial commit of NerdDinner web application as at SVN rev: 69463

---
+++ NerdDinner/Global.asax.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using NerdDinner.Helpers;
+
+namespace NerdDinner
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                    "PrettyDetails",
+                    "{Id}",
+                        new { controller = "Dinners", action = "Details" },
+                        new { Id = @"\d+" }
+                    );
+
+            routes.MapRoute(
+                    "UpcomingDinners",
+                    "Dinners/Page/{page}",
+                    new { controller = "Dinners", action = "Index" }
+            );
+
+            routes.MapRoute(
+                    "Default",                                              // Route name
+                    "{controller}/{action}/{id}",                           // URL with parameters
+                    new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "OpenIdDiscover",
+                "Auth/Discover");
+
+        }
+
+        public override void Init()
+        {
+            this.AuthenticateRequest += new EventHandler(MvcApplication_AuthenticateRequest);
+            this.PostAuthenticateRequest += new EventHandler(MvcApplication_PostAuthenticateRequest);
+            base.Init();
+        }
+
+        void Application_Start()
+        {
+            RegisterRoutes(RouteTable.Routes);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new MobileCapableRazorViewEngine());
+        }
+
+        void MvcApplication_PostAuthenticateRequest(object sender, EventArgs e)
+        {
+            HttpCookie authCookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null)
+            {
+                string encTicket = authCookie.Value;
+                if (!String.IsNullOrEmpty(encTicket))
+                {
+                    FormsAuthenticationTicket ticket = FormsAuthentication.Decrypt(encTicket);
+                    NerdIdentity id = new NerdIdentity(ticket);
+                    GenericPrincipal prin = new GenericPrincipal(id, null);
+                    HttpContext.Current.User = prin;
+                }
+            }
+        }
+
+        void MvcApplication_AuthenticateRequest(object sender, EventArgs e)
+        {
+        }
+    }
+}
